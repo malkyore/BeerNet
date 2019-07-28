129a1449728affc15fdcd7f262dd0b16c5e9585a
malkyore <nickschwartzrox@gmail.com>
2019-07-28 18:26:56 -0500

    Added UserSettings model and endpoints.

---
+++ BeerNet/Models/UserSettings.cs
@@ -0,0 +1,22 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BeerNet.Models
+{
+    public class userSettings : userSettingsBase
+    {
+        public ObjectId Id { get; set; }
+        public new string idString
+          {
+              get
+              {
+                  return Id.ToString();
+              }
+          }
+
+    }
+}
---
+++ BeerNet/Models/UserSettingsBase.cs
@@ -0,0 +1,23 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BeerNet.Models
+{
+    public class userSettingsBase 
+    {
+        [BsonElement("userID")]
+        public string userID { get; set; }
+        [BsonElement("firstName")]
+        public string firstName { get; set; }
+        [BsonElement("lastName")]
+        public string lastName { get; set; }
+        [BsonElement("theme")]
+        public string theme { get; set; }
+        [BsonElement("userCreatedDate")]
+        public DateTimeOffset userCreatedDate { get; set; }
+    }
+}
